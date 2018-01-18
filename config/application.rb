2d3728f74d54fbad015bd03ed7d08a0eeeb83d40
j-eaves <jdeaves@gmail.com>
2018-01-18 10:03:29 -0500

    modified precompile in production.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Runthejewels03
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # This was added later by me
+    # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
